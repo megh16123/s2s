3162e94fb9f693950315c07ef9f316bb2fbb6a58
megh16123 <meghansh2002@gmail.com>
2022-03-20 16:00:46 +0530

    Backend Started

---
+++ models/student.js
@@ -0,0 +1,40 @@
+const mongoose  = require('mongoose');
+
+const studentDat = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    fatherName:{
+        type:String,
+        required:true
+    },
+    class:{
+      type:String,
+      required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    Address:{
+        type:String,
+        required:true
+    },
+    contact:{
+        type:Number,
+        maxlength:[12,"Contact cannot exceed 10 numbers"],
+        required:true
+    },
+    Fcontact:{
+        type:Number,
+        maxlength:[12,"Contact cannot exceed 10 numbers"],
+        required:true    
+    },
+    fee:{
+        type:Number,
+        required:true
+    }
+});
+
+module.exports = mongoose.model("Student",studentDat);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    MONGO_URI:"mongodb+srv://developer:s2s123@cluster0.vzzcx.mongodb.net/s2s?retryWrites=true&w=majority"
+  }
 }
 
 module.exports = nextConfig
